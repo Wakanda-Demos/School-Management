8eb407323337bc1b9169ab0b648b3e15ed93a224
IDRISSI Mohamed <mohamed.idrissi@4d.com>
2013-07-03 10:14:02 +0100

    Update handlers.js

--- School-Management Project/BootStraps/handlers.js
+++ School-Management Project/BootStraps/handlers.js
@@ -8,7 +8,4 @@ directory.setLoginListener("loginHandler");
 addHttpRequestHandler('/logmein' , 'Scripts/handlersImpl.js' , 'loginMe');
 addHttpRequestHandler('/getCal' , 'Scripts/handlersImpl.js' , 'exportCal');
 
-// Temporary handlers :
-addHttpRequestHandler('^/$' , 'Scripts/handlersImpl.js' , 'index');
-
 new SharedWorker("Workers/initApp.js", "InitApp");
